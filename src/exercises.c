919a1b20fa632f0400f92e7ff6d061ff2a1972f8
Jacobo <jacoboduranaristizabal@gmail.com>
2025-04-27 09:03:27 -0500

    Se adelantaron los primeros 7 ejercicios

--- src/exercises.c
+++ src/exercises.c
@@ -6,37 +6,75 @@
 
 int calcSum(int arr[], int n)
 {
-    // Escriba aquí su código
+    int suma = 0;
+    for(int i = 0; i < n; i++){
+        suma = suma + arr[i];
+    }
+    return suma;
 }
 
 int findMax(int arr[], int n)
 {
-    // Escriba aquí su código
+    int max = arr[0];
+    for(int i = 1; i < n; i++){
+        if(arr[i] > max){
+            max = arr[i];
+        }
+    }
+    return max;
 }
 
 float calcAverage(int arr[], int n)
 {
-    // Escriba aquí su código
+    int suma = 0;
+    float prom;
+    for(int i = 0; i < n; i++){
+        suma = suma + arr[i];
+    }
+    prom = suma / n;
+    return prom;
 }
 
 int countEvens(int arr[], int n)
 {
-    // Escriba aquí su código
+    int cont = 0;
+    for(int i = 0; i < n; i++){
+        if(arr[i] % 2 == 0){
+            cont++;
+        }
+    }
+    return cont;
 }
 
 int sumFirstLast(int arr[], int n)
 {
-    // Escriba aquí su código
+    int FirstLast;
+
+    FirstLast = arr[0] + arr[n - 1];
+
+    return FirstLast;
 }
 
 int findMin(int arr[], int n)
 {
-    // Escriba aquí su código
+    int min = arr[0];
+    for(int i = 1; i < n; i++){
+        if(arr[i] < min){
+            min = arr[i];
+        }
+    }
+    return min;
 }
 
 int subtractArraysSum(int a[], int b[], int n)
 {
-    // Escriba aquí su código
+    int total = 0;
+    int dif;
+    for(int i = 0; i < n; i++){
+        dif = a[i] - b[i];
+        total = dif + total;
+    }
+    return total;
 }
 
 int mergeArraysSum(int a[], int n1, int b[], int n2)
